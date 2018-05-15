11ddcd1461c904890032e1c9ef55abd9e62a3d65
Ryan Williams <rwilliams@mla.org>
2018-05-15 16:49:27 -0400

    first

---
+++ hc-notifications.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     HC Notifications
+ * Plugin URI:      https://github.com/mlaa/hc-notifications.git
+ * Description:     HC Notifications
+ * Author:          MLA
+ * Author URI:      https://github.com/mlaa
+ * Text Domain:     hc-notifications
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hc_Notifications
+ */
+
+// Your code starts here.

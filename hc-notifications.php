9509923d558cd70f34219fcb756266be8adec6f0
Ryan Williams <rwilliams@mla.org>
2018-05-16 14:13:47 -0400

    add component & first two notifications

--- hc-notifications.php
+++ hc-notifications.php
@@ -12,4 +12,78 @@
  * @package         Hc_Notifications
  */
 
-// Your code starts here.
+/**
+ * Bootstrap the component.
+ */
+function hc_notifications_init_component() {
+	require_once 'includes/class-hc-notifications-component.php';
+	buddypress()->hc_notifications = new HC_Notifications_Component();
+}
+add_action( 'bp_loaded', 'hc_notifications_init_component' );
+
+/**
+ * Send new users a notification telling them to review their default notification settings.
+ *
+ * @param int $user_id User ID.
+ */
+function hc_notifications_user_register( $user_id ) {
+	$action = 'new_user_notification_settings';
+
+	add_filter( "hc_notifications_{$action}_link", function() {
+		return trailingslashit( bp_loggedin_user_domain() . bp_get_settings_slug() ) . 'notifications';
+	} );
+
+	add_filter( "hc_notifications_{$action}_text", function() {
+		return 'Welcome! Be sure to review your email preferences.';
+	} );
+
+	$notification_id = bp_notifications_add_notification(
+		[
+			'user_id'          => $user_id,
+			'component_name'   => 'hc_notifications',
+			'component_action' => $action,
+		]
+	);
+}
+add_action( 'user_register', 'hc_notifications_user_register' );
+
+/**
+ * Send new group site members a notification about their new role.
+ *
+ * @param int $group_id ID of the group.
+ * @param int $user_id  Optional. ID of the user. Defaults to the currently
+ *                      logged-in user.
+ * @return bool True on success, false on failure.
+ */
+function hc_notifications_groups_join_group( $group_id, $user_id ) {
+	$action  = 'new_group_site_member';
+	$blog_id = get_groupblog_blog_id( $group_id );
+	switch_to_blog( $blog_id );
+	$blog_name = get_bloginfo( 'name' );
+	$caps = array_keys( get_user_meta( $user_id, 'wp_capabilities', true ) );
+	$role = $caps[0]; // Just report the first role for now.
+	restore_current_blog();
+
+	add_filter( "hc_notifications_{$action}_link", function() use ( $blog_id ) {
+		return get_site_url( $blog_id );
+	} );
+
+	add_filter( "hc_notifications_{$action}_text", function() use ( $blog_name, $role ) {
+		return sprintf(
+			'You\'ve been added to the group site "%s" with the role of %s.',
+			$blog_name,
+			$role
+		);
+	} );
+
+	$notification_id = bp_notifications_add_notification(
+		[
+			'user_id'          => $user_id,
+			'component_name'   => 'hc_notifications',
+			'component_action' => $action,
+			'item_id'          => $group_id,
+			'allow_duplicate'  => true,
+		]
+	);
+}
+add_action( 'groups_join_group', 'hc_notifications_groups_join_group', 10, 2 );
---
+++ includes/class-hc-notifications-component.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * @package Hc_Notifications
+ */
+
+class HC_Notifications_Component extends BP_Component {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct() {
+		parent::start(
+			'hc_notifications',
+			'HC Notifications',
+			plugin_dir_path( __DIR__ )
+		);
+
+		add_filter( 'bp_notifications_get_registered_components', [ $this, 'filter_registered_components' ] );
+	}
+
+	public function setup_globals() {
+		parent::setup_globals( [
+			'notification_callback' => [ $this, 'format_notifications' ],
+		] );
+	}
+
+	/**
+	 * Notification formatter callback.
+	 *
+	 * @param string $action            The kind of notification being rendered.
+	 * @param int    $item_id           The primary item id.
+	 * @param int    $secondary_item_id The secondary item id.
+	 * @param int    $total_items       The total number of messaging-related notifications
+	 *                                  waiting for the user.
+	 * @param string $format            Return value format. 'string' for BuddyBar-compatible
+	 *                                  notifications; 'array' for WP Toolbar. Default: 'string'.
+	 * @return string|array Formatted notifications.
+	 */
+	public function format_notifications( $action, $item_id, $secondary_item_id, $total_items, $format ) {
+		$link = apply_filters_ref_array( "hc_notifications_${action}_link", func_get_args() );
+		$text = apply_filters_ref_array( "hc_notifications_${action}_text", func_get_args() );
+
+		if ( 'new_user_notification_settings' === $action ) {
+			$link = trailingslashit( bp_loggedin_user_domain() . bp_get_settings_slug() ) . 'notifications';
+			$text = 'Welcome! Be sure to review your email preferences.';
+		}
+
+		if ( 'string' === $format ) {
+			$content = sprintf(
+				'<a href="%1$s" title="%2$s">%2$s</a>',
+				$link,
+				$text
+			);
+		} else {
+			$content = [
+				'text' => $text,
+				'link' => $link,
+			];
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Register this component.
+	 *
+	 * @param array $component_names Array of registered component names.
+	 */
+	public function filter_registered_components( $component_names ) {
+		$component_names[] = buddypress()->hc_notifications->id;
+		return $component_names;
+	}
+
+}

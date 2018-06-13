e77a6f114d8d23797e9925bd0a77868e6d7cc7ce
Ryan Williams <rwilliams@mla.org>
2018-06-13 14:29:05 -0400

    ensure deleting forum notification does not result in another one next action

--- includes/class-hc-notification-join-mla-forum.php
+++ includes/class-hc-notification-join-mla-forum.php
@@ -24,19 +24,28 @@ class HC_Notification_Join_MLA_Forum extends HC_Notification {
 		$add_notification = function( $group_id, $user_id ) {
 			$mla_oid = groups_get_groupmeta( $group_id, 'mla_oid' );
 
-			// Only add notifications for MLA forums.
-			if ( ! $mla_oid ) {
+			// Avoid sending this type of notification more than once per user per group by setting a user meta flag.
+			$meta_key   = sprintf( '%s_%s_%s_sent', buddypress()->hc_notifications->id, self::$action, $group_id );
+			$meta_value = get_user_meta( $user_id, $meta_key, true );
+
+			if (
+				$meta_value ||
+				! $mla_oid ||
+				'M' !== substr( groups_get_groupmeta( $group_id, 'mla_oid' ), 0, 1 )
+			) {
 				return;
 			}
 
 			$result = bp_notifications_add_notification(
 				[
 					'user_id'          => $user_id,
-					'component_name'   => 'hc_notifications',
+					'component_name'   => buddypress()->hc_notifications->id,
 					'component_action' => self::$action,
 					'item_id'          => $group_id,
 				]
 			);
+
+			add_user_meta( $user_id, $meta_key, true, true );
 		};
 
 		add_action( 'groups_join_group', $add_notification, 10, 2 );

ab97cf988536a710215f65cb85f483e3f7bf41a7
Ryan Williams <rwilliams@mla.org>
2018-06-13 14:56:31 -0400

    ensure deleting newsletter notification does not result in another one next login

--- includes/class-hc-notification-newsletter-opt-out.php
+++ includes/class-hc-notification-newsletter-opt-out.php
@@ -22,20 +22,26 @@ class HC_Notification_Newsletter_Opt_Out extends HC_Notification {
 	 */
 	public static function setup_actions() {
 		$add_notification = function() {
-			$meta_value = get_user_meta( get_current_user_id(), 'newsletter_optin', true );
+			$optin_meta_value = get_user_meta( get_current_user_id(), 'newsletter_optin', true );
 
-			// Only add notifications for users who have not opted in.
-			if ( 'no' !== $meta_value ) {
+			// Avoid sending this type of notification more than once per user per group by setting a user meta flag.
+			$sent_meta_key   = sprintf( '%s_%s_sent', buddypress()->hc_notifications->id, self::$action );
+			$sent_meta_value = get_user_meta( get_current_user_id(), $sent_meta_key, true );
+
+			// Only add notifications for users who have not opted in and haven't already been notified.
+			if ( 'no' !== $optin_meta_value || $sent_meta_value ) {
 				return;
 			}
 
 			$result = bp_notifications_add_notification(
 				[
 					'user_id'          => get_current_user_id(),
-					'component_name'   => 'hc_notifications',
+					'component_name'   => buddypress()->hc_notifications->id,
 					'component_action' => self::$action,
 				]
 			);
+
+			add_user_meta( get_current_user_id(), $sent_meta_key, true, true );
 		};
 
 		add_action( 'wp_login', $add_notification );

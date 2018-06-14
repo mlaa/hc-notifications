ac409d043e705c668ada8ef74e3b09f7d479f003
Ryan Williams <rwilliams@mla.org>
2018-06-14 11:57:56 -0400

    add duplicate prevention for join group site

--- includes/class-hc-notification-join-group-site.php
+++ includes/class-hc-notification-join-group-site.php
@@ -28,8 +28,12 @@ class HC_Notification_Join_Group_Site extends HC_Notification {
 		$add_notification = function( $group_id, $user_id ) {
 			$blog_id = groups_get_groupmeta( $group_id, 'groupblog_blog_id' );
 
-			// Bail if this group has no blog.
-			if ( ! $blog_id ) {
+			// Avoid sending this type of notification more than once per user per group by setting a user meta flag.
+			$meta_key   = sprintf( '%s_%s_%s_sent', buddypress()->hc_notifications->id, self::$action, $group_id );
+			$meta_value = get_user_meta( $user_id, $meta_key, true );
+
+			// Bail if this group has no blog or we've already sent this notification before.
+			if ( $meta_value || ! $blog_id ) {
 				return;
 			}
 
@@ -42,6 +46,8 @@ class HC_Notification_Join_Group_Site extends HC_Notification {
 					'secondary_item_id' => $blog_id,
 				]
 			);
+
+			add_user_meta( $user_id, $meta_key, true, true );
 		};
 
 		add_action( 'groups_join_group', $add_notification, 10, 2 );
--- tests/test-class-hc-notification-join-group-site.php
+++ tests/test-class-hc-notification-join-group-site.php
@@ -50,4 +50,51 @@ class HC_Notification_Join_Group_Site_Test extends BP_UnitTestCase {
 		$this->assertSame( 1, $count );
 	}
 
+	/**
+	 * Ensure deleting a notification does not result in the same type of notification being added later.
+	 */
+	public function test_permanent_delete() {
+		if ( ! is_multisite() ) {
+			$this->assertTrue( true );
+			return;
+		}
+
+		$u = $this->factory()->user->create();
+		$g = $this->factory()->group->create();
+		$b = $this->factory()->blog->create();
+
+		groups_update_groupmeta( $g, 'groupblog_blog_id', $b );
+
+		groups_join_group( $g, $u );
+
+		$notifications = bp_notifications_get_all_notifications_for_user( $u );
+
+		foreach ( $notifications as $n ) {
+			if (
+				buddypress()->hc_notifications->id === $n->component_name &&
+				HC_Notification_Join_Group_Site::$action === $n->component_action
+			) {
+				// Bypass the permission check in bp_notifications_delete_notification() and delete directly.
+				BP_Notifications_Notification::delete( array( 'id' => $n->id ) );
+			}
+		}
+
+		groups_leave_group( $g, $u );
+		groups_join_group( $g, $u );
+
+		$notifications = bp_notifications_get_all_notifications_for_user( $u );
+
+		$count = 0;
+
+		foreach ( $notifications as $n ) {
+			if (
+				buddypress()->hc_notifications->id === $n->component_name &&
+				HC_Notification_Join_Group_Site::$action === $n->component_action
+			) {
+				$count++;
+			}
+		}
+
+		$this->assertSame( 0, $count );
+	}
 }
